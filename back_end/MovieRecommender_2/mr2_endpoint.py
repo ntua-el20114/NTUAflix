ade87b0f856fc8ba19eca7ae268a6cc9d825df7e
ntua-el19204 <el19204@mail.ntua.gr>
2024-01-27 15:31:05 +0200

    Movie Recommender 2 (by genre)

---
+++ back_end/MovieRecommender_2/mr2_endpoint.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource
+import pandas as pd
+from io import StringIO
+import secrets
+from back_end import db
+from ..auth import login_required, admin_required, getTitleObject
+import json
+
+from back_end.MovieRecommender_2.recommend_by_genre import recommend_by_genre
+
+class Movie_Recommender_2(Resource):
+    @login_required
+    def get(self, user, genre, username):
+        return_list = recommend_by_genre(genre, username)
+        final_list = []
+        for item in return_list:
+            id = item["id"]
+            result = getTitleObject(id)
+            final_list.append(result)
+
+        return_dict = {}
+        return_dict["result"] = final_list
+        json_result = json.dumps(return_dict, sort_keys=False)
+        response = make_response(json_result, 200)
+        return response
