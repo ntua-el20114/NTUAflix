dfc96661f3e63d9788c0a8818019f63038da9b5a
ntua-el20114 <el20114@mail.ntua.gr>
2024-01-30 01:08:11 +0200

    UserProfile page now shows liked and disliked movies

--- front-end/flask_app/routes/UserProfile.py
+++ front-end/flask_app/routes/UserProfile.py
@@ -1,11 +1,34 @@
-from flask import Blueprint, render_template, redirect, session
+from flask import Blueprint, render_template, redirect, session, flash
 import requests
 
 user_profile = Blueprint('UserProfile', __name__)
 
 @user_profile.route('/UserProfile', methods=['GET'])
 def show():
-    return render_template('UserProfile.html')
+    # Get Liked Movies
+    url = 'http://127.0.0.1:9876/ntuaflix_api/getlikedmovies'
+    response = requests.get(url, headers = {'X-OBSERVATORY-AUTH': session['user_token']})
+    if response.status_code == 200:
+        print("Successfully got liked movies")
+        # Keep only essensial data
+        liked_movies = [{"title": movie["originalTitle"], "thumbnail": movie["titlePoster"]} for movie in response.json()["result"]]
+        print(liked_movies)
+    else:
+        flash("There was an error getting liked movies", "error")
+
+    # Get Disliked Movies
+    url = 'http://127.0.0.1:9876/ntuaflix_api/getdislikedmovies'
+    response = requests.get(url, headers = {'X-OBSERVATORY-AUTH': session['user_token']})
+    if response.status_code == 200:
+        print("Successfully got disliked movies")
+        disliked_movies = [{"title": movie["originalTitle"], "thumbnail": movie["titlePoster"]} for movie in response.json()["result"]]
+        print(disliked_movies)
+    else:
+        flash("There was an error getting disliked movies", "error")
+    
+
+    return render_template('UserProfile.html', 
+                           liked_movies=liked_movies, disliked_movies=disliked_movies)
 
 @user_profile.route('/UserProfile', methods=['POST'])
 def logout():
