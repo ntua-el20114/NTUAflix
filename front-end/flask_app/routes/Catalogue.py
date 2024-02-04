60186d1ddf8a2bf76cb73f6ef8827305ae2043b5
ntua-el20114 <el20114@mail.ntua.gr>
2024-02-04 23:24:27 +0200

    Frontend - movie recommendations in catalogue page

--- front-end/flask_app/routes/Catalogue.py
+++ front-end/flask_app/routes/Catalogue.py
@@ -5,14 +5,50 @@ catalogue = Blueprint('Catalogue', __name__)
 
 @catalogue.route('/Catalogue', methods=['GET'])
 def show():
-    # Get top rated movies !-Not working
+    # Get liked movies
+    recommended = []
+    url = 'http://127.0.0.1:9876/ntuaflix_api/getlikedmovies'
+    response = requests.get(url, headers = {'X-OBSERVATORY-AUTH': session['user_token']})
+    if response.status_code == 200:
+        too_long = False
+        # Get recommendations based on each movie
+        print("Successfully got liked movies")
+        for movie in response.json()["result"]:
+            if too_long: break
+            print("\nSIMILAR TO: ", movie["originalTitle"])
+            url = f'http://127.0.0.1:9876/ntuaflix_api/movierecommender_1/{movie["originalTitle"]}'
+            response = requests.get(url, headers = {'X-OBSERVATORY-AUTH': session['user_token']})
+            try:
+                for title in response.json()['result']:
+                    if too_long: break
+                    # avoid duplicate movies
+                    exists = False
+                    for rec in recommended:
+                        if rec['id'] == title['titleID']:
+                            exists = True
+                            break
+                    print(title['originalTitle'])
+                    if not exists: recommended.append({"title": title["originalTitle"], "thumbnail": title["titlePoster"], "id": title["titleID"]})
+                    
+                    # If recommendations list gets too long, keep only the best rec for each movie
+                    if len(recommended) > 30: break
+                    
+                    # If recommendations list gets way too long, break completely
+                    # This is done to handle edge cases where the user has a very large amount of liked movies
+                    if len(recommended) >= 60:
+                        too_long = True
+                        break
+            except:
+                continue
+    else:
+        flash("There was an error getting recommended movies", "error")
+
+    # Get top rated movies
     url = 'http://127.0.0.1:9876/ntuaflix_api/gettopratedmovies'
     response = requests.get(url, headers = {'X-OBSERVATORY-AUTH': session['user_token']})
     if response.status_code == 200:
-        print("Successfully got top rated movies")
         top_rated = [{"title": movie["originalTitle"], "thumbnail": movie["titlePoster"], "id": movie["titleID"]} 
             for movie in response.json()["result"]]
-        print(top_rated)
     else:
         flash("There was an error getting top rated movies", "error")
-    return render_template('Catalogue.html', top_rated=top_rated)
+    return render_template('Catalogue.html', top_rated=top_rated, recommended=recommended)
